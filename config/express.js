5bdc05d3836deba9e180712861f594108ed1e440
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-09-16 22:59:53 +0530

    Update express.js

---
+++ config/express.js
@@ -0,0 +1,20 @@
+// express.js
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const app = express();
+
+// Configure session middleware
+app.use(
+  session({
+    secret: 'your-secret-key', // Replace with a strong secret key
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: require('./database') }),
+  })
+);
+
+// Add middleware and routes here
+
+module.exports = app;
